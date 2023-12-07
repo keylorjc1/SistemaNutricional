56d8c71c439380bde579d028f8c20aa1ddf519c8
keylorjc1 <106986264+keylorjc1@users.noreply.github.com>
2023-12-06 18:09:21 -0600

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaNutricional.Startup))]
+namespace SistemaNutricional
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

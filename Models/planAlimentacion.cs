fca643b38ba97a00873fd2549d26022264a7ce94
Elizabeth Quesada Cordero <saulpumis@gmail.com>
2024-02-05 12:23:11 -0600

    ConexionBD
    Conexion exitosa a la base de datos, con cada uno de los modelos

---
+++ Models/planAlimentacion.cs
@@ -0,0 +1,24 @@
+namespace SistemaNutricional.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("planAlimentacion")]
+    public partial class planAlimentacion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int idPlan { get; set; }
+
+        [StringLength(100)]
+        public string comida { get; set; }
+
+        [StringLength(100)]
+        public string alimentos { get; set; }
+
+        public int? calorias { get; set; }
+    }
+}
